b073e0bedb1d2115bd06cbaa775a670f0b571c69
luki120 <luki120@exerra.xyz>
2025-04-13 20:04:49 -0300

    fixed website link

--- site/src/jsMain/kotlin/me/luki/website/utils/Constants.kt
+++ site/src/jsMain/kotlin/me/luki/website/utils/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val GITHUB_URL = "https://github.com/Luki120"
     const val KOBWEB_URL = "https://github.com/varabyte/kobweb"
     const val TWITTER_URL = "https://twitter.com/Lukii120"
-    const val WEBSITE_URL = "https://github.com/Luki120/Website"
+    const val WEBSITE_URL = "https://github.com/Luki120/luki120.xyz"
 }
